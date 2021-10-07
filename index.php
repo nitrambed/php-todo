08fb82dd48bc7fcaddd4e98849511d466fcae286
nitrambed <nitrambed@protonmail.com>
2021-10-07 16:19:56 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script async src="index.js"></script>
+    <title>Todo</title>
+</head>
+
+<body>
+    <header>HEADER</header>\
+    <div class="content">CONTENT</div>
+    <footer>FOOTER</footer>
+
+</body>
+
+</html>
