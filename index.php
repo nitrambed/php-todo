046c29b3c266cbeaea6f608ed826b688fb2c2d8b
nitrambed <nitrambed@protonmail.com>
2021-10-08 14:02:57 +0200

    Form logic

--- index.php
+++ index.php
@@ -12,19 +12,42 @@
   <title>Todo</title>
 </head>
 
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $todo = $_POST['todo'] ?? '';
+
+  if (!$todo) {
+    $error = ERROR_REQUIRED;
+  } else if (mb_strlen($todo) < 5) {
+    $error = ERROR_TOO_SHORT;
+  }
+}
+?>
+
+
 <body>
   <div class="container">
-    <header>
-      <div class="logo">Dyma Todo</div>
-    </header>
+    <?php require_once 'includes/header.php' ?>
     <div class="content">
       <div class="todo-container">
         <h1>Ma Todo</h1>
-        <div class="todo-form"></div>
+        <form class="todo-form" action="/" method="post">
+          <input name="todo" type="text">
+          <button class="btn btn-primary">Ajouter</button>
+        </form>
+        <?php if ($error) : ?>
+          <p class="text-danger"><?= $error ?></p>
+        <?php endif; ?>
         <div class="todo-list"></div>
       </div>
+
     </div>
-    <footer>2011 / 2021 © Tous droits réservés</footer>
+    <?php require_once 'includes/footer.php' ?>
   </div>
 </body>
 
