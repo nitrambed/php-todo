dbee369200ee5dcd4d6e41b2cc02874fef2128db
nitrambed <nitrambed@protonmail.com>
2021-10-08 18:28:02 +0200

    CRUD todo

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php
+const ERROR_REQUIRED = 'Veuillez renseigner une todo';
+const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
+$filename = __DIR__ . "/data/todos.json";
+$error = '';
+$todo = '';
+$todos = [];
+
+
+if (file_exists($filename)) {
+  $data = file_get_contents($filename);
+  $todos = json_decode($data, true) ?? [];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+  $todo = $_POST['todo'] ?? '';
+
+  if (!$todo) {
+    $error = ERROR_REQUIRED;
+  } else if (mb_strlen($todo) < 5) {
+    $error = ERROR_TOO_SHORT;
+  }
+
+  if (!$error) {
+    $todos = [...$todos, [
+      'name' => $todo,
+      'done' => false,
+      'id' => time()
+    ]];
+    file_put_contents($filename, json_encode($todos));
+    header('Location: /');
+  }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,24 +49,6 @@
   <title>Todo</title>
 </head>
 
-<?php
-const ERROR_REQUIRED = 'Veuillez renseigner une todo';
-const ERROR_TOO_SHORT = 'Veuillez entrer au moins 5 caractères';
-$error = '';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-  $todo = $_POST['todo'] ?? '';
-
-  if (!$todo) {
-    $error = ERROR_REQUIRED;
-  } else if (mb_strlen($todo) < 5) {
-    $error = ERROR_TOO_SHORT;
-  }
-}
-?>
-
-
 <body>
   <div class="container">
     <?php require_once 'includes/header.php' ?>
@@ -37,13 +56,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <div class="todo-container">
         <h1>Ma Todo</h1>
         <form class="todo-form" action="/" method="post">
-          <input name="todo" type="text">
+          <input value="<?= $todo ?>" name="todo" type="text">
           <button class="btn btn-primary">Ajouter</button>
         </form>
         <?php if ($error) : ?>
           <p class="text-danger"><?= $error ?></p>
         <?php endif; ?>
-        <div class="todo-list"></div>
+        <ul class="todo-list">
+          <?php foreach ($todos as $t) : ?>
+            <li class="todo-item <?= $t['done'] ? 'low-opacity' : '' ?>">
+              <span class="todo-name"><?= $t['name'] ?></span>
+              <a href="/edit-todo.php?id=<?= $t['id'] ?>">
+                <button class="btn btn-primary btn-small"><?= $t['done'] ? 'Annuler' : 'Valider' ?></button>
+              </a>
+              <a href="/remove-todo.php?id=<?= $t['id'] ?>">
+                <button class="btn btn-danger btn-small">Supprimer</button>
+              </a>
+            </li>
+          <?php endforeach; ?>
+        </ul>
       </div>
 
     </div>
